8cd53f025a4fe29f8fd9d66566a6a4077015ca0d
Kelvi077 <kelvisaraci20@gmail.com>
2024-10-27 12:03:41 +0100

    update2

--- script.js
+++ script.js
@@ -1,44 +1,36 @@
-$(document).ready(function(){
-    let getData = localStorage.getItem('getData')
+$(document).ready(function () {
+  let getData = localStorage.getItem("getData");
 
-    if (getData){
-        let list = JSON.parse(getData);
-        for (let i = 0; i<list.length; i++){
-            $('ul').append('<li>' + list[i] + '</li>');
-        }
-
+  if (getData) {
+    let list = JSON.parse(getData);
+    for (let i = 0; i < list.length; i++) {
+      $("ul").append("<li>" + list[i] + "</li>");
     }
+  }
 });
 
-
-$('.add-button').on('click', add);
+$(".add-button").on("click", add);
 
 let list;
 
-
 function add() {
-    let data = $('.add').serializeArray(); // Move data retrieval here
-    let getData = localStorage.getItem('getData')
+  let data = $(".add").serializeArray(); // Move data retrieval here
+  let getData = localStorage.getItem("getData");
 
-    if (getData){
-        list = JSON.parse(getData);
-    }
-    else{
-        list = []
-    }
+  if (getData) {
+    list = JSON.parse(getData);
+  } else {
+    list = [];
+  }
 
-    if (data[0].value.trim() !== "") {
-        $('ul').append('<li>' + data[0].value + '</li>');
-        $('.add').val(''); // Clear the input after adding
+  if (data[0].value.trim() !== "") {
+    $("ul").append("<li>" + data[0].value + "</li>");
+    $(".add").val(""); // Clear the input after adding
 
-        list.push(data[0].value);
+    list.push(data[0].value);
 
-        localStorage.setItem('getData', JSON.stringify(list));
-    } else {
-        alert("Please enter a value before adding to the list.");
-    }
+    localStorage.setItem("getData", JSON.stringify(list));
+  } else {
+    alert("Please enter a value before adding to the list.");
+  }
 }
-
-
-
-
