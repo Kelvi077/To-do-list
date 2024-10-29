489a4af4ec238bc8e9178c5fb8ac31638a7e8e47
Kelvi077 <kelvisaraci20@gmail.com>
2024-10-29 16:44:16 +0100

    add x button

--- script.js
+++ script.js
@@ -4,18 +4,19 @@ $(document).ready(function () {
   if (getData) {
     let list = JSON.parse(getData);
     for (let i = 0; i < list.length; i++) {
-      $("ul").append("<li>" + list[i] + "</li>");
+      $("ul").append(
+        "<li>" + list[i] + "<button class='remove-btn'>X</button></li>"
+      );
     }
   }
 });
 
 $(".add-button").on("click", add);
 
-let list;
-
 function add() {
-  let data = $(".add").serializeArray(); // Move data retrieval here
+  let inputValue = $(".add").val().trim(); // Directly get the input value
   let getData = localStorage.getItem("getData");
+  let list;
 
   if (getData) {
     list = JSON.parse(getData);
@@ -23,14 +24,26 @@ function add() {
     list = [];
   }
 
-  if (data[0].value.trim() !== "") {
-    $("ul").append("<li>" + data[0].value + "</li>");
+  if (inputValue !== "") {
+    $("ul").append(
+      "<li>" + inputValue + "<button class='remove-btn'>X</button></li>"
+    );
+
     $(".add").val(""); // Clear the input after adding
 
-    list.push(data[0].value);
-
-    localStorage.setItem("getData", JSON.stringify(list));
+    list.push(inputValue);
+    localStorage.setItem("getData", JSON.stringify(list)); // Store the updated list in localStorage
   } else {
     alert("Please enter a value before adding to the list.");
   }
 }
+
+// Delegated event listener for remove buttons
+$(document).on("click", ".remove-btn", function () {
+  let itemText = $(this).parent().text().replace("X", "").trim();
+  let getData = JSON.parse(localStorage.getItem("getData"));
+  getData = getData.filter((item) => item !== itemText);
+
+  localStorage.setItem("getData", JSON.stringify(getData));
+  $(this).parent().remove();
+});
